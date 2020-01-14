b9b7ed98793a09ce242ff777ff0e96dbb79ae129
Alexander Stuckenholz <alexander.stuckenholz@hshl.de>
2020-01-14 09:51:17 +0100

    Added error handling to weather data import

--- DataImport/weather_import.py
+++ DataImport/weather_import.py
@@ -24,41 +24,47 @@ with urlopen(zipurl) as zipresp:
     with ZipFile(BytesIO(zipresp.read())) as zfile:
         zfile.extractall()
 
-cols = ['S_ID', 'Standort', 'Geo_Breite', 'Geo_Laenge', 'Hoehe', 'Betreiber']
-df = pd.read_csv("wetterdaten_Wetterstation.csv", sep=";", encoding = "ISO-8859-1", decimal=",", usecols=cols)
-df.columns = ['s_id','standort','geo_breite','geo_laenge','hoehe','betreiber']
-df['s_id'] = df['s_id'].astype(int)
-df['geo_breite'] = df['geo_breite'].astype(float)
-df['geo_laenge'] = df['geo_laenge'].astype(float)
-df['hoehe'] = df['hoehe'].astype(float)
-df.to_sql('wetterstation', engine, index=False, if_exists='replace', schema='wetter')
+try:
+	cols = ['S_ID', 'Standort', 'Geo_Breite', 'Geo_Laenge', 'Hoehe', 'Betreiber']
+	df = pd.read_csv("wetterdaten_Wetterstation.csv", sep=";", encoding = "ISO-8859-1", decimal=",", usecols=cols)
+	df.columns = ['s_id','standort','geo_breite','geo_laenge','hoehe','betreiber']
+	df['s_id'] = df['s_id'].astype(int)
+	df['geo_breite'] = df['geo_breite'].astype(float)
+	df['geo_laenge'] = df['geo_laenge'].astype(float)
+	df['hoehe'] = df['hoehe'].astype(float)
+	df.to_sql('wetterstation', engine, index=False, if_exists='replace', schema='wetter')
+except:
+	print("Import der Wetterstationen fehlgeschlagen!")
 
-df = pd.read_csv("wetterdaten_Wettermessung.csv", sep=";",
-    encoding = "ISO-8859-1", decimal=",", 
-    usecols={"Stations_ID", "Datum", "Qualitaet", "Min_5cm", "Min_2m", "Mittel_2m",
-    "Max_2m","Relative_Feuchte","Mittel_Windstaerke","Max_Windgeschwindigkeit",
-    "Sonnenscheindauer","Mittel_Bedeckungsgrad","Niederschlagshoehe","Mittel_Luftdruck"})
-df.columns = ['stations_id','datum','qualitaet','min_5cm','min_2m','mittel_2m',
-    "max_2m","relative_feuchte","mittel_windstaerke","max_windgeschwindigkeit",
-    "sonnenscheindauer","mittel_bedeckungsgrad","niederschlagshoehe","mittel_luftdruck"]
-df['stations_id'] = df['stations_id'].astype(int)
-df['datum'] = pd.to_datetime(df['datum'])
-df['qualitaet'] = df['qualitaet'].astype(pd.Int32Dtype())
-df['min_5cm'] = df['min_5cm'].astype(float)
-df['min_2m'] = df['min_2m'].astype(float)
-df['mittel_2m'] = df['mittel_2m'].astype(float)
-df['max_2m'] = df['max_2m'].astype(float)
-df['relative_feuchte'] = df['relative_feuchte'].astype(float)
-df['mittel_windstaerke'] = df['mittel_windstaerke'].astype(float)
-df['max_windgeschwindigkeit'] = df['max_windgeschwindigkeit'].astype(float)
-df['sonnenscheindauer'] = df['sonnenscheindauer'].astype(float)
-df['mittel_bedeckungsgrad'] = df['mittel_bedeckungsgrad'].astype(float)
-df['niederschlagshoehe'] = df['niederschlagshoehe'].astype(float)
-df['mittel_luftdruck'] = df['mittel_luftdruck'].astype(float)
-df.to_sql('wettermessung', engine, index=False, if_exists='replace', schema='wetter')
+try:
+	df = pd.read_csv("wetterdaten_Wettermessung.csv", sep=";",
+		encoding = "ISO-8859-1", decimal=",", 
+		usecols={"Stations_ID", "Datum", "Qualitaet", "Min_5cm", "Min_2m", "Mittel_2m",
+		"Max_2m","Relative_Feuchte","Mittel_Windstaerke","Max_Windgeschwindigkeit",
+		"Sonnenscheindauer","Mittel_Bedeckungsgrad","Niederschlagshoehe","Mittel_Luftdruck"})
+	df.columns = ['stations_id','datum','qualitaet','min_5cm','min_2m','mittel_2m',
+		"max_2m","relative_feuchte","mittel_windstaerke","max_windgeschwindigkeit",
+		"sonnenscheindauer","mittel_bedeckungsgrad","niederschlagshoehe","mittel_luftdruck"]
+	df['stations_id'] = df['stations_id'].astype(int)
+	df['datum'] = pd.to_datetime(df['datum'])
+	df['qualitaet'] = df['qualitaet'].astype(pd.Int32Dtype())
+	df['min_5cm'] = df['min_5cm'].astype(float)
+	df['min_2m'] = df['min_2m'].astype(float)
+	df['mittel_2m'] = df['mittel_2m'].astype(float)
+	df['max_2m'] = df['max_2m'].astype(float)
+	df['relative_feuchte'] = df['relative_feuchte'].astype(float)
+	df['mittel_windstaerke'] = df['mittel_windstaerke'].astype(float)
+	df['max_windgeschwindigkeit'] = df['max_windgeschwindigkeit'].astype(float)
+	df['sonnenscheindauer'] = df['sonnenscheindauer'].astype(float)
+	df['mittel_bedeckungsgrad'] = df['mittel_bedeckungsgrad'].astype(float)
+	df['niederschlagshoehe'] = df['niederschlagshoehe'].astype(float)
+	df['mittel_luftdruck'] = df['mittel_luftdruck'].astype(float)
+	df.to_sql('wettermessung', engine, index=False, if_exists='replace', schema='wetter')
+except:
+	print("Import der Wettermessungen fehlgeschlagen!")
 
-os.remove("wetterdaten_Wettermessung.csv")
-os.remove("wetterdaten_Wetterstation.csv")
+#os.remove("wetterdaten_Wettermessung.csv")
+#os.remove("wetterdaten_Wetterstation.csv")
 
 with engine.connect() as con:
     con.execute('alter table wetter.wetterstation add column location geography;')
